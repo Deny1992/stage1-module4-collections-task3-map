85dc6401a2009a8be06d449dcdec452a71230b00
Denyonepeny <denisshulga1992@gmail.com>
2023-01-18 13:45:31 +0300

    done

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,21 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for(Map.Entry<Integer, String> entry : sourceMap.entrySet()){
+            if(map.containsKey(entry.getValue())) {
+                Integer newValue = entry.getKey();
+                Integer currentValue = map.get(entry.getValue());
+                map.put(entry.getValue(),
+                        newValue < currentValue ? newValue : currentValue);
+            }else {
+                map.put(entry.getValue(), entry.getKey());
+            }
+        }
+        return map;
     }
 }
